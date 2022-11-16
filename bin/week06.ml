a2f64fc246d1eae59ac0343ab79857d62eb955dd
Ryosuke SATO <ryosuke@kb.is.s.u-tokyo.ac.jp>
2022-11-16 13:58:24 +0900

    Add missing file

---
+++ bin/week06.ml
@@ -0,0 +1,43 @@
+open Util
+open Config
+open Assignment
+open Check
+
+let init () =
+  exec
+    [change_directory Dir.tmp;
+     clone Group;
+     find_compiler_directory Group;
+     build Group;
+     check_compiler_exists Group]
+  |> Option.to_list
+
+let output_of file =
+  let name = file.name in
+  [Format.sprintf "test/%s.out" name;
+   Format.sprintf "test/%s.err" name]
+
+let toi4 () =
+  let dir = !!Dir.archive ^ "/test" in
+  FileUtil.mkdir ~parent:true dir;
+  let files =
+    [{name = "fun"; content = Raw Testcases.fun_};
+     {name = "partial"; content = Raw Testcases.partial}]
+  in
+  let check file () =
+    let output = output_of file in
+    match run_compiler ~dir ~output file () with
+    | None ->
+        let filename = Printf.sprintf "%s/%s.s" dir file.name in
+        if Sys.file_exists filename then
+          []
+        else
+          [Output_not_found "*.s"]
+    | Some e -> [e]
+  in
+  concat_map check files ()
+
+let assignments : t =
+  {init;
+   check_commit_files = true;
+   items = [4, Group, toi4]}
